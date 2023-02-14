2c0d892efb08c0873162bf2d9aa625bf5055a4a1
Joe Grinstead <npm@mailforce.net>
2023-02-14 10:35:03 -0500

    Added a qrcode from a user input.
    That's pretty cool, am I right?

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 import './style.css'
+import {makeQrCode} from "@grinstead/makeqrcode";
 
 document.querySelector('#app').innerHTML = `
   <div>
-    Goodbye, Paul!
+    <label for="myinput">Put some text here:</label>
+    <input type="text" id="myinput"></input>
+    <div id="qrcode">Input Text!</div>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+const input = document.getElementById("myinput");
+
+input.addEventListener("input", event => {
+  updateQr(input.value);
+});
+
+function updateQr(value) {
+  const { sideLength, path } = makeQrCode("L", value);
+
+  document.getElementById("qrcode").innerHTML = `
+  <svg width="320" height="320" viewBox="0 0 ${sideLength} ${sideLength}" xmlns="http://www.w3.org/2000/svg">
+    <style>path {stroke: black;} background {color: white;}</style>
+    <path d="${path}" />
+  </svg>`;
+}
